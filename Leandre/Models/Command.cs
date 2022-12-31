cc918d911b6ecc05b9519251d87866882ed93821
Simpenzwe Honore Leandre <leandrehonore650@gmail.com>
2022-12-31 13:55:48 +0530

    initial commit

---
+++ Leandre/Models/Command.cs
@@ -0,0 +1,12 @@
+namespace Leandre.Models
+{   
+     public class Command
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; } = default!;
+        public string Line {get; set; } = default!;
+
+        public string Platform { get; set; } = default!;
+    }
+
+}
